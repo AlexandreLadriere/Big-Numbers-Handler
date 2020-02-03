b5f26390de9bc1262986f54ddf654fb5fe5bf26a
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-02-03 16:16:18 +0100

    add: Adding test file and isEqual/isGreater functions

--- src/BigInt.java
+++ src/BigInt.java
@@ -9,41 +9,97 @@ public class BigInt {
 
     /**
      * Creates a BigInt object
+     *
      * @param representation Representation of the BigInt object (List<Integer>)
      */
     BigInt(List<Integer> representation) {
+        // check size
         ini();
         this.copy(representation);
+    }
+
+    BigInt(int[] representation) {
+        // check size
+        ini();
+        for(int i = 0; i < this.representation.size(); i++) {
+            this.representation.set(i, representation[i]);
+        }
+    }
+
+    /**
+     * Compares the calling BigInt to the one given in parameter
+     * @param b BigInt to compare (BigInt)
+     * @return Result of the comparison (true: this >= b, false: b > this) (boolean)
+     */
+    public boolean isGreater(BigInt b) {
+        //check size
+        boolean isGreater = false;
+        int i = 0;
+        while (i < this.representation.size()) {
+            if (this.representation.get(i) > b.getRepresentation().get(i)) {
+                isGreater = true;
+                i =this.representation.size();
+            } else if (this.representation.get(i) < b.getRepresentation().get(i)) {
+                i = this.representation.size();
+            }
+            i++;
+        }
+        return isGreater;
+    }
+
+    /**
+     * Checks if two BigInt are equals
+     * @param b The BigInt to compare to (BigInt)
+     * @return Boolean to indicate if equals or not (true: equals; false: not equals) (boolean)
+     */
+    public boolean isEqual(BigInt b) {
+        // check size
+        boolean isEq = true;
+        for(int i = 0; i < this.representation.size(); i++) {
+            if (!this.representation.get(i).equals(b.getRepresentation().get(i))) {
+                isEq = false;
+                break;
+            }
+        }
+        return isEq;
     }
 
     /**
      * Initializes the representation to 0
      */
     private void ini() {
-        for(int i = 0; i < bitLenght/blockSize; i++) {
-            representation.set(i, 0);
+        for (int i = 0; i < bitLenght / blockSize; i++) {
+            representation.add(0);
         }
     }
 
-    // public add()
+    /*
+    public BigInt add(BigInt b, BigInt mod) {
+        int carry = 0;
+
+    }
+    */
 
     /**
      * Copies a BigInt object into the calling BigInt
+     *
      * @param toCopy The BigInt object to copy into the current BigInt (BigInt)
      */
     public void copy(BigInt toCopy) {
-        for(int i = 0; i < toCopy.representation.size(); i++) {
+        //check size
+        for (int i = 0; i < toCopy.getRepresentation().size(); i++) {
             this.representation.set(i, toCopy.representation.get(i));
         }
     }
 
     /**
      * Returns a string representation of the object
+     *
      * @return String representation of the object (String)
      */
     public String toString() {
         StringBuilder str = new StringBuilder("BigInt[");
-        for(int i = 0; i < this.representation.size(); i++) {
+        for (int i = 0; i < this.representation.size(); i++) {
             str.append(this.representation.toString());
             str.append(" ");
         }
@@ -52,6 +108,7 @@ public class BigInt {
 
     /**
      * Gets the representation
+     *
      * @return The object representation (List<Integer>)
      */
     public List<Integer> getRepresentation() {
@@ -60,6 +117,7 @@ public class BigInt {
 
     /**
      * Sets the representation
+     *
      * @param representation New representation (List<Integer>)
      */
     public void setRepresentation(List<Integer> representation) {
@@ -68,10 +126,12 @@ public class BigInt {
 
     /**
      * Copies an integer list into the representation list
+     *
      * @param toCopy The integer to copy into the representation (List<Integer>)
      */
     private void copy(List<Integer> toCopy) {
-        for(int i = 0; i < toCopy.size(); i++) {
+        // check size
+        for (int i = 0; i < toCopy.size(); i++) {
             this.representation.set(i, toCopy.get(i));
         }
     }
--- src/Test.java
+++ src/Test.java
@@ -1,6 +1,34 @@
+import java.util.Arrays;
+
 public class Test {
 
-    public static void main(String[] args) {
+    int[] a = new int[8];
+    int[] b = new int[8];
+    int[] p = new int[8];
+
+    public Test() {
+        iniVariables();
+        BigInt A = new BigInt(a);
+        BigInt B = new BigInt(b);
+
+        System.out.println(A.isGreater(B));
+        System.out.println(A.isEqual(B));
     }
 
+    public static void main(String[] args) {
+        new Test();
+    }
+
+    private void iniVariables() {
+        iniArrayToZero(a);
+        iniArrayToZero(b);
+        a[6] = 3;
+        b[6] = 3;
+        a[7] = 7;
+        b[7] = 7;
+    }
+
+    private void iniArrayToZero(int[] array) {
+        Arrays.fill(array, 0);
+    }
 }
