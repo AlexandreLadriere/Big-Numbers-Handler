6669318388b480ed6f9fab4728d699a0148b1872
AlexandreLadriere <alexandre.ladriere77@gmail.com>
2020-02-05 10:31:19 +0100

    add: Adding IntArray to BigInt conversion to python test file

--- test.py
+++ test.py
@@ -1,4 +1,4 @@
-def intToString(a, bit = 256):
+def intToBitString(a, bit = 256):
     """
     Converts an integer to a binary string representation (without '0b')
     :param a: The integer to convert (integer)
@@ -10,10 +10,10 @@ def intToString(a, bit = 256):
 
 def bitStringtoSigned(str, block_size = 32):
     """
-    Inserts each block_size step a zero in order to represent
-    a string of block_size signed integer in binary format from the 
+    Inserts each :block_size: step a zero in order to represent
+    a string of :block_size: signed integer in binary format from the 
     original string (loss of information)
-    :param str: Original string of an integer representation in binary format (String)
+    :param str: Original string of the integer in binary format (String)
     :param block_size: step for the insertion of 0 (Integer)
     :return: string of block_size signed integer in binary format (String)
     """
@@ -22,27 +22,23 @@ def bitStringtoSigned(str, block_size = 32):
     reverse_str = str[::-1]
     for i in range(0, len(str), block_size-1):
         cpt += 1
-        print("reverse_block =", reverse_str[i:i+block_size-1] + '0')
         signed_str += reverse_str[i:i+block_size-1] + '0'
     signed_str = signed_str[::-1]
     signed_str = signed_str[cpt:]
-    print("signed_str =", signed_str)
     return signed_str
 
 def bitStringToIntArray(str, block_size = 32):
     """
-    Converts a string of a represention in binary format of
-    an integer into an array of integer of block_size-1 bit-size
+    Converts a string of an integer in binary format (:block_size:bits signed, e.g. "0111 0010")
+    into an array of integer of block_size-1 bit-size
     :param str: Original string to convert into an array of integer (String)
     :param block_size: Size of blocks (Integer)
-    :return: Representation of an integer array of the given string representation
+    :return: Representation of an integer array of the given string representation (Integer array)
     """
     int_array = []
     signed_str = bitStringtoSigned(str, block_size)
     for i in range(0, len(signed_str), block_size):
-        print("block =", signed_str[i:i+block_size])
         tmpStr = signed_str[i:i+block_size]
-        print(len(tmpStr))
         int_array.append(int(tmpStr, 2))
     return int_array
 
@@ -50,17 +46,34 @@ def convertBigIntToIntArray(big_int, bit = 256, block_size = 32):
     """
     Converts a big integer into an array representation. Each array is coded
     on 2^:block_size:-1 bits and the representation has a total length of :bit: bits
-    :param big_int: The integer you want to convert (integer)
-    :param bit: The length in bits of the representation (integer)
-    :param block_size: The initial size of each integer in the final array in bits (integer)
-    :return: An array representation of the integer
+    e.g. [0, 0, 0, 0, 0, 0, 500, 2]
+    :param big_int: The integer you want to convert (Integer)
+    :param bit: The length in bits of the representation (Integer)
+    :param block_size: The initial size of each integer in the final array in bits (Integer)
+    :return: An array representation of the integer (Integer Array)
     """
-    str = intToString(big_int, bit)
+    str = intToBitString(big_int, bit)
     int_array = bitStringToIntArray(str)
     return int_array
 
+def convertIntArraytoBigInt(int_array, block_size = 32):
+    """
+    Converts an integer array (of :block_size: bits, signed) into an integer
+    (e.g. [0, 0, 0, 0, 0, 0, 1, 0] --> 2147483648)
+    :param int_array: The integer array you want to convert (Integer array)
+    :param block_size: Size, in bits, of the length of each integer in the array (Integer)
+    :return: Integer representation of the array (Integer)
+    """
+    bin_str = ""
+    for i in range(len(int_array)):
+        bin_str += intToBitString(int_array[i], block_size)[1:]
+    big_int = int(bin_str, 2)
+    return big_int
+
 if __name__ == "__main__":
-    str_test = intToString(2147483647*2147483647)
-    print(str_test)
-    int_array_test = bitStringToIntArray(str_test)
-    print(int_array_test)
+    max = 2147483647 # 2^31 (signed)
+    A = [0, 0, 0, 0, 0, 0, 1, 0]
+    B = [0, 0, 0, 0, 0, 0, 0, max]
+    P = [0, 0, 0, 2, 0, 0, 1, 0]
+    print(convertBigIntToIntArray(max + 1))
+    print(convertIntArraytoBigInt(A))
